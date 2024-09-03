6dee2c036b28f0c0924a43050e191a865c9fe330
Phantasus15 <andrericardo151001@gmail.com>
2024-09-03 00:12:42 -0300

    Criação do header

---
+++ header.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-expand-lg">
+        <img src="assets/logo.jfif" style="height:75px;">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Inicio<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Pedidos<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Agendamentos<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Estoque<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Histórico<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Relatório<span class="sr-only">(current)</span></a>
+                </li>
+            </ul>
+        </div>
+        <div class="perfil">
+            <ul class="navbar-nav mr-auto centro">
+                <li class="nav-item active">
+                    Perfil
+                </li>
+                <li class="nav-item active">
+                    <i class="bi bi-person-circle"></i>
+                </li>
+            </ul>
+        </div>
+    </nav>
