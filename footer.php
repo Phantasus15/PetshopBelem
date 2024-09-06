8c3b230d1098977b29f998a613be1744ece6fbfe
Phantasus15 <andrericardo151001@gmail.com>
2024-09-05 21:30:43 -0300

    Criação do componente footer

---
+++ footer.php
@@ -0,0 +1,23 @@
+<link rel="stylesheet" href="css/footer.css">
+<div class="footer">
+    <div class="lista-nav">
+
+        <h3>Aqui você encontra:</h3>
+
+
+        <a>
+            Atendedimento
+        </a>
+
+
+        <a>
+            Meus Pedidos
+        </a>
+
+
+        <a>
+            Meu Cadastro
+        </a>
+    </div>
+    <img class="imagem-roda" src="assets/logo.jfif" alt="">
+</div>
